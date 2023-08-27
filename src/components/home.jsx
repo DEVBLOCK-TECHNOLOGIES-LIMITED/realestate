c58a52e8c0bff94b397750dd6f8602142743f874
ojibona1 <ojibona01@gmail.com>
2023-08-27 13:54:20 -0700

    m

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -10,7 +10,7 @@ function Home(){
     return(
         <div className="home">
             <div className="banner">
-                <h1>Agents. Tours. Loans. Homes.</h1>
+                <h1>Agents. Tours. Short-Stay. Homes.</h1>
                 <div className="search">
                 <input type="text" placeholder="Search with home features & locations"/>
                 <BiSearch size={30}/>
